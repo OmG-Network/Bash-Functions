077268e0152e79ff932a825958df9ff76670212c
[OmG] Network <webmaster@omg-network.de>
2018-03-30 15:26:02 +0200

    Update check_arch.sh

--- check_os/check_arch.sh
+++ check_os/check_arch.sh
@@ -2,20 +2,9 @@ function check_arch ()
 {
 if [ -x /usr/bin/arch ]; then
         CPU_ARCH=$(/usr/bin/arch)
-    # Check x64
-        if [ $CPU_ARCH == "x86_64" ]; then
-            echo "Supported"
-        fi
-    # Check x86
-        if [ $CPU_ARCH == "x86" ]; then
-            echo "Not Supported"
-        fi
-    # Check ARM7
-        if [ $CPU_ARCH == "armv71" ]; then
-            echo "Supported"
-        fi
 else
         CPU_ARCH=$(lscpu | grep Architecture | awk '{print $2}')
+fi
     # Check x64
         if [ $CPU_ARCH == "x86_64" ]; then
             echo "Supported"
@@ -28,5 +17,4 @@ else
         if [ $CPU_ARCH == "armv71" ]; then
             echo "Supported"
         fi
-fi
 }
